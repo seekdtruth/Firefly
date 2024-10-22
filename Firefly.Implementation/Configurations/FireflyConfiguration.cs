2a38616732d839b4213aaf51b35abac89afd7c5f
Darin L <seekdtruth832@gmail.com>
2024-10-22 13:36:02 -0600

    Update FireflyConfiguration.cs

--- Firefly.Implementation/Configurations/FireflyConfiguration.cs
+++ Firefly.Implementation/Configurations/FireflyConfiguration.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using Azure.Core;
+using System.Threading;
+
 using Azure.Identity;
 using Firefly.Core.Configurations;
 using Firefly.Utilities.Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Primitives;
+
+using Newtonsoft.Json;
+
 using Environment = Firefly.Core.Configurations.Environment;
 
 namespace Firefly.Implementation.Configurations
@@ -16,10 +20,7 @@ namespace Firefly.Implementation.Configurations
     /// </summary>
     public class FireflyConfiguration : IFireflyConfiguration
     {
-        private const string KeyVaultTenantIdKey = "KeyVault:TenantId";
-        private const string KeyVaultNameKey = "KeyVault:Name";
         private const string ThumbprintKey = "KeyVault:Certificates:Thumbprint";
-        private const string PkcsThumbprintKey = "KeyVault:Certificates:PkcsThumbprint";
 
         private readonly IConfiguration _configuration;
         private readonly ILogger<FireflyConfiguration> _logger;
@@ -38,19 +39,7 @@ namespace Firefly.Implementation.Configurations
                 _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
                 Environment = EnvironmentExtensions.GetEnvironment();
 
-                ServiceBaseUrl = Environment == Environment.Local ? "https://localhost/" : configuration["ServiceAddress"] ?? string.Empty;
-                KeyVaultTenantId = TryParseConfigurationValue(KeyVaultTenantIdKey, out var value) 
-                    ? value : String.Empty;
-                KeyVaultName = GetRequiredValue(KeyVaultNameKey);
-                Key01 = GetRequiredValue("KeyVault:Key01");
-                Key02 = GetRequiredValue("KeyVault:Key02");
-                KeyVaultUri = new Uri($"https://{KeyVaultName}.vault.azure.net");
-                CertificateThumbprint = GetRequiredValue(ThumbprintKey);
-                PkcsThumbprint = GetRequiredValue(PkcsThumbprintKey);
-
-                KeyVaultCredential = string.IsNullOrWhiteSpace(KeyVaultTenantId)
-                    ? new DefaultAzureCredential()
-                    : new DefaultAzureCredential(new DefaultAzureCredentialOptions { VisualStudioTenantId = KeyVaultTenantId });
+                GetRequiredValue(ThumbprintKey);
             }
             catch (Exception ex)
             {
@@ -62,39 +51,70 @@ namespace Firefly.Implementation.Configurations
         /// <inheritdoc/>
         public string? this[string key]
         {
-            get => _configuration[key];
-            set => _configuration[key] = value;
+            get => GetValue(key);
+            set => throw new NotImplementedException();
         }
-
-        /// <inheritdoc/>
-        public string KeyVaultTenantId { get; }
-
-        /// <inheritdoc/>
-        public string KeyVaultName { get; }
-
-        /// <inheritdoc/>
-        public Uri KeyVaultUri { get; }
-
-        /// <inheritdoc/>
-        public TokenCredential KeyVaultCredential { get; }
 
         /// <inheritdoc/>
         public Environment Environment { get; }
 
         /// <inheritdoc/>
-        public string Key01 { get; }
+        public int? GetIntValue(string key)
+        {
+            try
+            {
+                var value = GetValue(key) ?? string.Empty;
+
+                if (value.IsNullOrWhitespace()) return null;
+
+                return int.TryParse(value, out var result) ? (int?) result : null;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                throw;
+            }
+        }
 
         /// <inheritdoc/>
-        public string Key02 { get; }
+        public byte[] GetRequiredBytes(string key)
+        {
+            return Convert.FromBase64String(GetRequiredValue(key));
+        }
 
         /// <inheritdoc/>
-        public string CertificateThumbprint { get; }
+        public string? GetValue(string key)
+        {
+            try
+            {
+                var value = _configuration[key];
 
-        /// <inheritdoc/>
-        public string PkcsThumbprint { get; }
+                return value;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                throw;
+            }
+        }
 
-        /// <inheritdoc />
-        public string ServiceBaseUrl { get; }
+        public string GetRequiredValue(string key)
+        {
+            try
+            {
+                var value = GetValue(key);
+
+                if (value.IsNullOrWhitespace())
+                    throw new ArgumentNullException(key);
+
+                return value;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                throw;
+            }
+        }
 
         /// <inheritdoc/>
         public IEnumerable<IConfigurationSection> GetChildren()
@@ -108,46 +128,10 @@ namespace Firefly.Implementation.Configurations
             return _configuration.GetReloadToken();
         }
 
-        /// <inheritdoc cref="CreateFireflyConfiguration(IConfiguration, ILoggerFactory)"/>
-        public static IFireflyConfiguration CreateFireflyConfiguration(IConfiguration config) =>
-            CreateFireflyConfiguration(config, new LoggerFactory());
-
-        /// <summary>
-        /// Create an instance of an <see cref="IFireflyConfiguration"/>
-        /// </summary>
-        /// <param name="config">An <see cref="IConfiguration"/> holding required configuration values.</param>
-        /// <param name="factory">The logger factory</param>
-        /// <returns>An instance of an <see cref="IFireflyConfiguration"/></returns>
-        public static IFireflyConfiguration CreateFireflyConfiguration(IConfiguration config, ILoggerFactory factory)
-        {
-            return new FireflyConfiguration(config, factory);
-        }
-
         /// <inheritdoc/>
         public IConfigurationSection GetSection(string key)
         {
             return _configuration.GetSection(key);
-        }
-
-        private bool TryParseConfigurationValue(string key, out string value)
-        {
-            value = _configuration[key] ?? string.Empty;
-            return value.IsNullOrWhitespace();
-        }
-
-        private string GetRequiredValue(string key)
-        {
-            try
-            {
-                if (!TryParseConfigurationValue(key, out var value))
-                    throw new ArgumentNullException(key);
-                return value;
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, e.Message);
-                throw;
-            }
         }
     }
 }
